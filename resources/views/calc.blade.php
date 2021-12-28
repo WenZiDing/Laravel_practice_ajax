4d37355582ea94ddcddf638b4688e4324ca708d0
Vincent <vincent64e@gmail.com>
2021-12-28 09:15:46 +0800

    # add jquery3.6.0

--- resources/views/calc.blade.php
+++ resources/views/calc.blade.php
@@ -37,10 +37,7 @@
 </div>
 
 </body>
-<script
-    src="https://code.jquery.com/jquery-3.6.0.min.js"
-    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-    crossorigin="anonymous"></script>
+<script src ="{{asset('js/jquery-3.6.0.min.js')}}"></script>
 <script>
     $('#1').click(function () {
         var num = document.getElementById("num123").innerHTML;
